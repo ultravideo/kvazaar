5dcbe3e8df4dbd8e8dc7b7ffb9687f4527e5bbbb
Marko Viitanen <fador@iki.fi>
2012-06-04 11:53:48 +0300

    Data structure initialization functions

--- src/encoder.h
+++ src/encoder.h
@@ -8,28 +8,31 @@
 #ifndef _ENCODER_H
 #define _ENCODER_H
 
-
+//ToDo: add ME data
 typedef struct
 {
-   void (*IME)(encoder_control* encoder);
+  void (*IME)(encoder_control* encoder);
  
 } encoder_me;
 
 
 typedef struct
 {
-    FILE *file;
-
- 
+  FILE* file;
+  uint32_t width;
+  uint32_t height;
+  uint32_t height_in_LCU;
+  uint32_t width_in_LCU; 
 } encoder_input;
 
 typedef struct
 {
   encoder_input in;
   encoder_me me;
-  FILE *output; 
+  FILE* output; 
 } encoder_control;
 
-
+init_encoder_control(encoder_control* control,FILE* output) {control->output = output;};
+init_encoder_input(encoder_input* input,FILE* inputfile, uint32_t width, uint32_t height) {input->file = inputfile; input->width = width; input->height = height;};
 
 #endif
