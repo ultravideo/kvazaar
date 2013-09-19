4416d6ec36b61d66f43475375f8602369743f363
Ari Koivula <ari@koivu.la>
2013-09-19 15:29:39 +0300

    Refactor: filter.c/.h full cleanup.

--- src/filter.h
+++ src/filter.h
@@ -17,17 +17,29 @@
 #include "encoder.h"
 
 
+//////////////////////////////////////////////////////////////////////////
+// FUNCTIONS
+// Deblocking
+void filter_deblock_cu(encoder_control *encoder, int32_t x_cu, int32_t y_cu,
+                       int8_t depth, int32_t edge);
+void filter_deblock_edge_luma(encoder_control *encoder, 
+                              int32_t x_pos, int32_t y_pos, 
+                              int8_t depth, int8_t dir);
+void filter_deblock_edge_chroma(encoder_control *encoder, 
+                                int32_t xpos, int32_t ypos, 
+                                int8_t depth, int8_t dir);
+void filter_deblock(encoder_control *encoder);
+void filter_deblock_luma(uint8_t *src, int32_t offset, int32_t tc , int8_t sw,
+                         int8_t part_p_nofilter, int8_t part_q_nofilter,
+                         int32_t thr_cut, 
+                         int8_t filter_second_p, int8_t filter_second_q);
+void filter_deblock_chroma(uint8_t *src, int32_t offset, int32_t tc,
+                           int8_t part_p_nofilter, int8_t part_q_nofilter);
+// SAO
+
+//////////////////////////////////////////////////////////////////////////
+// MACROS
 #define EDGE_VER 0
 #define EDGE_HOR 1
-
-/* DEBLOCKING */
-void filter_deblock_cu(encoder_control* encoder, int32_t x_cu, int32_t y_cu, int8_t depth, int32_t edge);
-void filter_deblock_edge_luma(encoder_control* encoder, int32_t x_pos, int32_t y_pos, int8_t depth, int8_t dir);
-void filter_deblock_edge_chroma(encoder_control* encoder,int32_t xpos, int32_t ypos, int8_t depth, int8_t dir);
-void filter_deblock(encoder_control* encoder);
-void filter_deblock_luma( uint8_t* src, int32_t offset, int32_t tc , int8_t sw, int8_t part_p_nofilter, int8_t part_q_nofilter, int32_t thr_cut, int8_t filter_second_p, int8_t filter_second_q);
-void filter_deblock_chroma( uint8_t* src, int32_t offset, int32_t tc ,int8_t part_p_nofilter, int8_t part_q_nofilter);
-
-/* SAO */
 
 #endif
