1e9c894eba4d164adae799360294deb1ee1999e1
Laurent Fasnacht <laurent.fasnacht@ces.ch>
2014-04-02 11:31:12 +0200

    Coding tree block raster and tile scanning conversion process, according to ITU-T Rec. H.265 (04/2013) 6.5.1

--- src/encoder.c
+++ src/encoder.c
@@ -117,12 +117,163 @@ int encoder_control_init(encoder_control * const encoder, const config * const c
   
   //Tiles
   encoder->tiles_enable = 0;
+#if USE_TILES
+  if (encoder->cfg->tiles_width_count > 0 || encoder->cfg->tiles_height_count > 0) {
+    int i, j; //iteration variables
+    const int num_ctbs = encoder->in.width_in_lcu * encoder->in.height_in_lcu;
+    int tileIdx, x, y; //iterations variable for 6-9
+
+    //Temporary pointers to allow encoder fields to be const
+    int32_t *tiles_col_width, *tiles_row_height, *tiles_ctb_addr_rs_to_ts, *tiles_ctb_addr_ts_to_rs, *tiles_tile_id, *tiles_col_bd, *tiles_row_bd;
+    
+    encoder->tiles_enable = 1;
+    
+    //Will be (perhaps) changed later
+    encoder->tiles_uniform_spacing_flag = 1;
+    
+    //tilesn[x,y] contains the number of _separation_ between tiles, whereas the encoder needs the number of tiles.
+    encoder->tiles_num_tile_columns = encoder->cfg->tiles_width_count + 1;
+    encoder->tiles_num_tile_rows = encoder->cfg->tiles_height_count + 1;
+    
+    tiles_col_width = MALLOC(int32_t, encoder->tiles_num_tile_columns);
+    tiles_row_height = MALLOC(int32_t, encoder->tiles_num_tile_rows);
+    
+    tiles_col_bd = MALLOC(int32_t, encoder->tiles_num_tile_columns + 1);
+    tiles_row_bd = MALLOC(int32_t, encoder->tiles_num_tile_rows + 1);
+    
+    tiles_ctb_addr_rs_to_ts = MALLOC(int32_t, num_ctbs);
+    tiles_ctb_addr_ts_to_rs = MALLOC(int32_t, num_ctbs);
+    
+    tiles_tile_id = MALLOC(int32_t, num_ctbs);
+    
+    //(6-3) and (6-4) in ITU-T Rec. H.265 (04/2013)
+    if (!encoder->cfg->tiles_width_split) {
+      for (i=0; i < encoder->tiles_num_tile_columns; ++i) {
+        tiles_col_width[i] = ((i+1) * encoder->in.width_in_lcu) / encoder->tiles_num_tile_columns -
+                                  i * encoder->in.width_in_lcu / encoder->tiles_num_tile_columns;
+      }
+    } else {
+      int32_t last_pos_in_px = 0;
+      tiles_col_width[encoder->tiles_num_tile_columns-1] = encoder->in.width_in_lcu;
+      for (i=0; i < encoder->tiles_num_tile_columns - 1; ++i) {
+        int32_t column_width_in_lcu = (cfg->tiles_width_split[i] - last_pos_in_px) / LCU_WIDTH;
+        last_pos_in_px = cfg->tiles_width_split[i];
+        tiles_col_width[i] = column_width_in_lcu;
+        tiles_col_width[encoder->tiles_num_tile_columns - 1] -= column_width_in_lcu;
+      }
+      encoder->tiles_uniform_spacing_flag = 0;
+    }
+    
+    if (!encoder->cfg->tiles_height_split) {
+      for (i=0; i < encoder->tiles_num_tile_rows; ++i) {
+        tiles_row_height[i] = ((i+1) * encoder->in.height_in_lcu) / encoder->tiles_num_tile_rows -
+                                   i * encoder->in.height_in_lcu / encoder->tiles_num_tile_rows;
+      }
+    } else {
+      int32_t last_pos_in_px = 0;
+      tiles_row_height[encoder->tiles_num_tile_rows-1] = encoder->in.height_in_lcu;
+      for (i=0; i < encoder->tiles_num_tile_rows - 1; ++i) {
+        int32_t row_height_in_lcu = (cfg->tiles_height_split[i] - last_pos_in_px) / LCU_WIDTH;
+        last_pos_in_px = cfg->tiles_height_split[i];
+        tiles_row_height[i] = row_height_in_lcu;
+        tiles_row_height[encoder->tiles_num_tile_rows - 1] -= row_height_in_lcu;
+      }
+      encoder->tiles_uniform_spacing_flag = 0;
+    }
+    
+    //(6-5) in ITU-T Rec. H.265 (04/2013)
+    tiles_col_bd[0] = 0;
+    for (i = 0; i < encoder->tiles_num_tile_columns; ++i) {
+      tiles_col_bd[i+1] = tiles_col_bd[i] + tiles_col_width[i];
+    }
+    
+    //(6-6) in ITU-T Rec. H.265 (04/2013)
+    tiles_row_bd[0] = 0;
+    for (i = 0; i < encoder->tiles_num_tile_rows; ++i) {
+      tiles_row_bd[i+1] = tiles_row_bd[i] + tiles_row_height[i];
+    }
+
+    //(6-7) in ITU-T Rec. H.265 (04/2013)
+    //j == ctbAddrRs
+    for (j = 0; j < num_ctbs; ++j) {
+      int tileX, tileY;
+      int tbX = j % encoder->in.width_in_lcu;
+      int tbY = j / encoder->in.width_in_lcu;
+      
+      for (i = 0; i < encoder->tiles_num_tile_columns; ++i) {
+        if (tbX >= tiles_col_bd[i]) tileX = i;
+      }
+      
+      for (i = 0; i < encoder->tiles_num_tile_rows; ++i) {
+        if (tbY >= tiles_row_bd[i]) tileY = i;
+      }
+      
+      tiles_ctb_addr_rs_to_ts[j] = 0;
+      for (i = 0; i < tileX; ++i) {
+        tiles_ctb_addr_rs_to_ts[j] += tiles_row_height[tileY] * tiles_col_width[i];
+      }
+      for (i = 0; i < tileY; ++i) {
+        tiles_ctb_addr_rs_to_ts[j] += encoder->in.width_in_lcu * tiles_row_height[i];
+      }
+      tiles_ctb_addr_rs_to_ts[j] += (tbY - tiles_row_bd[tileY]) * tiles_col_width[tileX] + 
+                                     tbX - tiles_col_bd[tileX];
+    }
+    
+    //(6-8) in ITU-T Rec. H.265 (04/2013)
+    //Make reverse map from tile scan to raster scan
+    for (j = 0; j < num_ctbs; ++j) {
+      tiles_ctb_addr_ts_to_rs[tiles_ctb_addr_rs_to_ts[j]] = j;
+    }
+    
+    //(6-9) in ITU-T Rec. H.265 (04/2013)
+    tileIdx = 0;
+    for (j=0; j < encoder->tiles_num_tile_rows; ++j) {
+      for (i=0; i < encoder->tiles_num_tile_columns; ++i) {
+        for (y = tiles_row_bd[j]; y < tiles_row_bd[j+1]; ++y) {
+          for (x = tiles_col_bd[i]; x < tiles_col_bd[i+1]; ++x) {
+            tiles_tile_id[tiles_ctb_addr_rs_to_ts[y * encoder->in.width_in_lcu + x]] = tileIdx;
+          }
+        }
+        ++tileIdx;
+      }
+    }
+
+    encoder->tiles_col_width = tiles_col_width;
+    encoder->tiles_row_height = tiles_row_height;
+    
+    encoder->tiles_row_bd = tiles_row_bd;
+    encoder->tiles_col_bd = tiles_col_bd;
+    
+    encoder->tiles_ctb_addr_rs_to_ts = tiles_ctb_addr_rs_to_ts;
+    encoder->tiles_ctb_addr_ts_to_rs = tiles_ctb_addr_ts_to_rs;
+    
+    encoder->tiles_tile_id = tiles_tile_id;
+
+
+
+  }
+  
+#endif //USE_TILES
   
   return 1;
 }
 
 int encoder_control_finalize(encoder_control * const encoder) {
   //Tiles
+#if USE_TILES
+  if (encoder->tiles_enable) {
+    FREE_POINTER(encoder->tiles_col_width);
+    FREE_POINTER(encoder->tiles_row_height);
+    
+    FREE_POINTER(encoder->tiles_col_bd);
+    FREE_POINTER(encoder->tiles_row_bd);
+    
+    FREE_POINTER(encoder->tiles_ctb_addr_rs_to_ts);
+    FREE_POINTER(encoder->tiles_ctb_addr_ts_to_rs);
+    
+    FREE_POINTER(encoder->tiles_tile_id);
+  }
+#endif //USE_TILES
   scalinglist_destroy(&encoder->scaling_list);
   
   return 1;
