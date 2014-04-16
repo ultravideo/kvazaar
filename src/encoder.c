9112cbb58cfb91d854b6ce44b6f16f25f499e75f
Laurent Fasnacht <laurent.fasnacht@ces.ch>
2014-04-16 08:09:10 +0200

    Generate and use static tables

--- src/encoder.c
+++ src/encoder.c
@@ -29,6 +29,7 @@
 #include <string.h>
 #include <assert.h>
 
+#include "tables.h"
 #include "config.h"
 #include "cabac.h"
 #include "picture.h"
@@ -44,8 +45,6 @@
 
 double g_lambda_cost[55];
 double g_cur_lambda_cost;
-const uint32_t* g_sig_last_scan[3][5];
-int8_t g_convert_to_bit[LCU_WIDTH + 1];
 int8_t g_bitdepth = 8;
 
 /* Local functions. */
@@ -55,163 +54,6 @@ static void encode_sao(const encoder_control * const encoder,
                        cabac_data *cabac,
                        unsigned x_lcu, uint16_t y_lcu,
                        sao_info *sao_luma, sao_info *sao_chroma);
-
-/**
- * Initialize g_sig_last_scan with scan positions for a transform block of
- * size width x height.
- */
-static void init_sig_last_scan(uint32_t *buff_d, uint32_t *buff_h,
-                               uint32_t *buff_v,
-                               int32_t width, int32_t height)
-{
-  uint32_t num_scan_pos  = width * width;
-  uint32_t next_scan_pos = 0;
-  int32_t  xx, yy, x, y;
-  uint32_t scan_line;
-  uint32_t blk_y, blk_x;
-  uint32_t blk;
-  uint32_t cnt = 0;
-
-  assert(width == height && width <= 32);
-
-  if (width <= 4) {
-    uint32_t *buff_tmp = buff_d;
-
-    for (scan_line = 0; next_scan_pos < num_scan_pos; scan_line++) {
-      int    primary_dim  = scan_line;
-      int    second_dim  = 0;
-
-      while (primary_dim >= width) {
-        second_dim++;
-        primary_dim--;
-      }
-
-      while (primary_dim >= 0 && second_dim < width) {
-        buff_tmp[next_scan_pos] = primary_dim * width + second_dim ;
-        next_scan_pos++;
-        second_dim++;
-        primary_dim--;
-      }
-    }
-  }
-
-  if (width > 4 && width <= 32) {
-    uint32_t num_blk_side = width >> 2;
-    uint32_t num_blks   = num_blk_side * num_blk_side;
-    uint32_t log2_width = g_to_bits[width];
-
-    for (blk = 0; blk < num_blks; blk++) {
-      uint32_t init_blk_pos = g_sig_last_scan_cg[log2_width][SCAN_DIAG][blk];
-      next_scan_pos   = 0;
-
-      {
-        uint32_t offset_y    = init_blk_pos / num_blk_side;
-        uint32_t offset_x    = init_blk_pos - offset_y * num_blk_side;
-        uint32_t offset_d    = 4 * (offset_x + offset_y * width);
-        uint32_t offset_scan = 16 * blk;
-
-        for (scan_line = 0; next_scan_pos < 16; scan_line++) {
-          int    primary_dim  = scan_line;
-          int    second_dim  = 0;
-
-          //TODO: optimize
-          while (primary_dim >= 4) {
-            second_dim++;
-            primary_dim--;
-          }
-
-          while (primary_dim >= 0 && second_dim < 4) {
-            buff_d[next_scan_pos + offset_scan] = primary_dim * width +
-                                                  second_dim + offset_d;
-            next_scan_pos++;
-            second_dim++;
-            primary_dim--;
-          }
-        }
-      }
-    }
-  }
-
-  if (width > 2) {
-    uint32_t num_blk_side = width >> 2;
-
-    for (blk_y = 0; blk_y < num_blk_side; blk_y++) {
-      for (blk_x = 0; blk_x < num_blk_side; blk_x++) {
-        uint32_t offset    = blk_y * 4 * width + blk_x * 4;
-
-        for (y = 0; y < 4; y++) {
-          for (x = 0; x < 4; x++) {
-            buff_h[cnt] = y * width + x + offset;
-            cnt ++;
-          }
-        }
-      }
-    }
-
-    cnt = 0;
-
-    for (blk_x = 0; blk_x < num_blk_side; blk_x++) {
-      for (blk_y = 0; blk_y < num_blk_side; blk_y++) {
-        uint32_t offset = blk_y * 4 * width + blk_x * 4;
-
-        for (x = 0; x < 4; x++) {
-          for (y = 0; y < 4; y++) {
-            buff_v[cnt] = y * width + x + offset;
-            cnt ++;
-          }
-        }
-      }
-    }
-  } else {
-    for (yy = 0; yy < height; yy++) {
-      for (xx = 0; xx < width; xx++) {
-        buff_h[cnt] = yy * width + xx;
-        cnt ++;
-  }
-      }
-
-    cnt = 0;
-
-    for (xx = 0; xx < width; xx++) {
-      for (yy = 0; yy < height; yy++) {
-        buff_v[cnt] = yy * width + xx;
-        cnt ++;
-      }
-    }
-  }
-}
-
-
-void init_tables(void)
-{
-  int i;
-  int c = 0;
-
-  memset( g_convert_to_bit,-1, sizeof( g_convert_to_bit ) );
-
-  for (i = 4; i < LCU_WIDTH; i *= 2) {
-    g_convert_to_bit[i] = (int8_t)c;
-    c++;
-  }
-
-  g_convert_to_bit[i] = (int8_t)c;
-
-  c = 2;
-  for (i = 0; i < 5; i++) {
-    uint32_t *sls0, *sls1, *sls2;
-    sls0 = (uint32_t*)malloc(c*c*sizeof(uint32_t));
-    sls1 = (uint32_t*)malloc(c*c*sizeof(uint32_t));
-    sls2 = (uint32_t*)malloc(c*c*sizeof(uint32_t));
-
-    init_sig_last_scan(sls0, sls1, sls2, c, c);
-    
-    g_sig_last_scan[0][i] = sls0;
-    g_sig_last_scan[1][i] = sls1;
-    g_sig_last_scan[2][i] = sls2;
-    
-    c <<= 1;
-  }
-}
 
 /*!
   \brief Initializes lambda-value for current QP
@@ -243,15 +85,6 @@ void init_lambda(const encoder_control * const encoder)
   g_cur_lambda_cost = lambda;
 }
 
-void free_tables(void)
-{
-  int i;
-  for (i = 0; i < 5; i++) {
-    FREE_POINTER(g_sig_last_scan[0][i]);
-    FREE_POINTER(g_sig_last_scan[1][i]);
-    FREE_POINTER(g_sig_last_scan[2][i]);
-  }
-}
 encoder_control *init_encoder_control(config *cfg)
 {
   encoder_control *enc_c    = NULL;
--- src/encoder.h
+++ src/encoder.h
@@ -30,6 +30,7 @@
 #include "bitstream.h"
 #include "cabac.h"
 #include "config.h"
+#include "tables.h"
 
 typedef struct {
   int32_t  scaling_list_dc   [SCALING_LIST_SIZE_NUM][SCALING_LIST_NUM];
@@ -112,9 +113,7 @@ typedef struct
   scaling_list scaling_list;
 } encoder_control;
 
-void init_tables(void);
 void init_lambda(const encoder_control * const encoder);
-void free_tables(void);
 encoder_control *init_encoder_control(config *cfg);
 void init_encoder_input(encoder_input *input, FILE* inputfile,
                         int32_t width, int32_t height);
@@ -144,7 +143,6 @@ void encode_block_residual(const encoder_control * const encoder,
 
 extern double g_lambda_cost[55];
 extern double g_cur_lambda_cost;
-extern int8_t g_convert_to_bit[LCU_WIDTH + 1];
 extern int8_t g_bitdepth;
 
 static const uint8_t g_group_idx[32] = {
@@ -156,86 +154,12 @@ static const uint8_t g_group_idx[32] = {
 static const uint8_t g_min_in_group[10] = {
   0, 1, 2, 3, 4, 6, 8, 12, 16, 24 };
 
-/**
- * List of mappings for coefficients within a transform block.
- * First index: scan pattern 0 = diagonal, 1 = horizontal, 2 = vertical
- * Second index: (log2 - 1) size of transform block. 2x2 .. 32x32
- */
-extern const uint32_t* g_sig_last_scan[3][5];
 
-/***
-  * List of indices for 4x4 coefficient groups within 8x8 transform block.
-  * First index: 0 = diagonal, 1 = vertical, 2 horizontal scan pattern.
-  * Second index: (log2 - 2) size of transform block. 4x4 .. 32x32
-  */
-static const uint32_t g_sig_last_scan_8x8[3][4] =
-{ {0, 2, 1, 3},
-  {0, 1, 2, 3},
-  {0, 2, 1, 3}
-};
-
-/***
-  * List of indices for 4x4 coefficient groups within 16x16 transform block.
-  */
-static const uint32_t g_sig_last_scan_16x16[16] = {
-  0,  4,  1,  8,
-  5,  2, 12,  9,
-  6,  3, 13, 10,
-  7, 14, 11, 15
-};
-
-/***
-  * List of indices for 4x4 coefficient groups within 32x32 transform block.
-  */
-static const uint32_t g_sig_last_scan_32x32[64] = {
-  0,   8,  1, 16,  9,  2, 24, 17,
-  10,  3, 32, 25, 18, 11,  4, 40,
-  33, 26, 19, 12,  5, 48, 41, 34,
-  27, 20, 13,  6, 56, 49, 42, 35,
-  28, 21, 14,  7, 57, 50, 43, 36,
-  29, 22, 15, 58, 51, 44, 37, 30,
-  23, 59, 52, 45, 38, 31, 60, 53,
-  46, 39, 61, 54, 47, 62, 55, 63
-};
-
-/**
- * List of pointers to coefficient group mappings.
- * First index: (log2 - 2) of transform block size
- * Second index: scan pattern 0 = diagonal, 1 = horizontal, 2 = vertical
- */
-static const uint32_t *const g_sig_last_scan_cg[4][3] = {
-  { g_sig_last_scan_8x8[0], g_sig_last_scan_8x8[1], g_sig_last_scan_8x8[2] },  // 4x4, only first element is used
-  { g_sig_last_scan_8x8[0], g_sig_last_scan_8x8[1], g_sig_last_scan_8x8[2] },
-  { g_sig_last_scan_16x16, 0, 0 },
-  { g_sig_last_scan_32x32, 0, 0 }
-};
-
-
-//4 8 16 32 64 128
-//0 1  2  3  4   5
-static const uint8_t g_to_bits[129] =
-{
-  0,
-  0,0,0,0,
-  0,0,0,1,
-  0,0,0,0,0,0,0,2,
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5
-};
-#define TOBITS(len) g_to_bits[len]
 
 
 #define C1FLAG_NUMBER 8 // maximum number of largerThan1 flag coded in one chunk
 #define C2FLAG_NUMBER 1 // maximum number of largerThan2 flag coded in one chunk
 
-enum COEFF_SCAN_TYPE
-{
-  SCAN_DIAG = 0, // up-right diagonal scan
-  SCAN_HOR,      // horizontal first scan
-  SCAN_VER       // vertical first scan
-};
 
 
 #endif
