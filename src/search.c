656b0a321be30680e94ef6c0e5630db713e9ed08
Ari Koivula <ari@koivu.la>
2014-05-15 18:23:48 +0300

    Add chroma mode to lcu_set_intra_mode.
    - This is needed for intra split.

--- src/search.c
+++ src/search.c
@@ -576,7 +576,7 @@ static void work_tree_copy_down(int x_px, int y_px, int depth, lcu_t work_tree[M
 }
 
 
-static void lcu_set_intra_mode(lcu_t *lcu, int x_px, int y_px, int depth, int tr_depth, int pred_mode, int part_mode)
+static void lcu_set_intra_mode(lcu_t *lcu, int x_px, int y_px, int depth, int tr_depth, int pred_mode, int chroma_mode, int part_mode)
 {
   const int width_cu = LCU_CU_WIDTH >> depth;
   const int x_cu = SUB_SCU(x_px) >> MAX_DEPTH;
@@ -605,6 +605,7 @@ static void lcu_set_intra_mode(lcu_t *lcu, int x_px, int y_px, int depth, int tr
       cu->intra[1].mode = pred_mode;
       cu->intra[2].mode = pred_mode;
       cu->intra[3].mode = pred_mode;
+      cu->intra[0].mode_chroma = chroma_mode;
       cu->part_size = part_mode;
       cu->tr_depth = tr_depth;
       cu->coded = 1;
@@ -891,6 +892,7 @@ static int search_cu(encoder_state * const encoder_state, int x, int y, int dept
     if (cur_cu->type == CU_INTRA) {
       lcu_set_intra_mode(&work_tree[depth], x, y, depth, cur_cu->tr_depth,
                          cur_cu->intra[PU_INDEX(x >> 2, y >> 2)].mode,
+                         cur_cu->intra[0].mode_chroma,
                          cur_cu->part_size);
       intra_recon_lcu(encoder_state, x, y, depth, &work_tree[depth]);
     } else if (cur_cu->type == CU_INTER) {
