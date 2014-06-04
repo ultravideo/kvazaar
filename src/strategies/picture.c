ea04bcd6a47aace5b95031091e7995e1bbb0d543
Laurent Fasnacht <laurent.fasnacht@ces.ch>
2014-06-04 08:19:57 +0200

    AltiVec support for SAD

--- src/strategies/picture.c
+++ src/strategies/picture.c
@@ -5,6 +5,9 @@
 #if COMPILE_INTEL_SSE2 && COMPILE_INTEL_SSE41
 #include "picture-sse41.c"
 #endif
+#if COMPILE_POWERPC_ALTIVEC
+#include "picture-altivec.c"
+#endif
 
 unsigned (*reg_sad)(const pixel * const data1, const pixel * const data2,
                     const int width, const int height, const unsigned stride1, const unsigned stride2);
@@ -13,6 +16,7 @@ unsigned (*reg_sad)(const pixel * const data1, const pixel * const data2,
 static int strategy_register_picture(void* opaque) {
   if (!strategy_register_picture_generic(opaque)) return 0;
   
+#if COMPILE_INTEL
   if (g_hardware_flags.intel_flags.sse2) {
 #if COMPILE_INTEL_SSE2
     if (!strategy_register_picture_sse2(opaque)) return 0;
@@ -23,5 +27,14 @@ static int strategy_register_picture(void* opaque) {
 #endif
     }
   }
+#endif //COMPILE_INTEL
+
+#if COMPILE_POWERPC
+  if (g_hardware_flags.powerpc_flags.altivec) {
+#if COMPILE_POWERPC_ALTIVEC
+    if (!strategy_register_picture_altivec(opaque)) return 0;
+#endif //COMPILE_POWERPC_ALTIVEC
+  }
+#endif //COMPILE_POWERPC
   return 1;
 }
