538c9ec85bcadf889b9ddb62643842b38d0ae59d
Ari Koivula <ari@koivu.la>
2014-07-28 12:31:26 +0300

    Add a benchmark for strategies.
    - usage: kvazaar_tests -v -s speed
    
    Conflicts:
    	build/kvazaar_tests/kvazaar_tests.vcxproj
    	build/kvazaar_tests/kvazaar_tests.vcxproj.filters
    	tests/tests_main.c

--- tests/tests_main.c
+++ tests/tests_main.c
@@ -5,6 +5,7 @@ GREATEST_MAIN_DEFS();
 extern SUITE(sad_tests);
 extern SUITE(intra_sad_tests);
 extern SUITE(satd_tests);
+extern SUITE(speed_tests);
 
 int main(int argc, char **argv)
 {
@@ -12,5 +13,12 @@ int main(int argc, char **argv)
   RUN_SUITE(sad_tests);
   RUN_SUITE(intra_sad_tests);
   RUN_SUITE(satd_tests);
+
+  if (greatest_info.suite_filter &&
+      greatest_name_match("speed", greatest_info.suite_filter))
+  {
+    RUN_SUITE(speed_tests);
+  }
+
   GREATEST_MAIN_END();
 }
