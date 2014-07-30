150a47435d60ec98ec4f71dd76679441f92a67dc
Ari Koivula <ari@koivu.la>
2014-07-30 15:26:20 +0300

    Add missing files.

---
+++ tests/test_strategies.c
@@ -0,0 +1,28 @@
+#include "test_strategies.h"
+
+#include "src/strategyselector.h"
+
+
+strategy_list strategies;
+
+
+void init_test_strategies()
+{
+  strategies.allocated = 0;
+  strategies.count = 0;
+  strategies.strategies = NULL;
+
+  // Init strategyselector because it sets hardware flags.
+  strategyselector_init();
+
+  // Collect all strategies to be tested.
+  if (!strategy_register_picture(&strategies)) {
+    fprintf(stderr, "strategy_register_picture failed!\n");
+    return;
+  }
+
+  if (!strategy_register_dct(&strategies)) {
+    fprintf(stderr, "strategy_register_partial_butterfly failed!\n");
+    return;
+  }
+}
---
+++ tests/test_strategies.h
@@ -0,0 +1,10 @@
+#ifndef TEST_STRATEGIES_H_
+#define TEST_STRATEGIES_H_
+
+#include "src/strategyselector.h"
+
+extern strategy_list strategies;
+
+void init_test_strategies();
+
+#endif // TEST_STRATEGIES_H_
