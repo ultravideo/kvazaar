80cbda364b96edb6bb404ef736fdae976d923787
Ari Koivula <ari@koivu.la>
2015-05-05 10:17:06 +0300

    Fix coverity warning.
    - Variable guards dead code. Although, maybe it will complain about the
      dead code now instead.

--- src/search.c
+++ src/search.c
@@ -419,14 +419,14 @@ static unsigned tz_search(const encoder_state_t * const state, unsigned depth,
 {
 
   //TZ parameters
-  int iSearchRange = 96;  // search range for each stage
-  int iRaster = 5;  // search distance limit and downsampling factor for step 3                   
-  unsigned step2_type = 0;  // search patterns for steps 2 and 4
-  unsigned step4_type = 0;
-  bool bRasterRefinementEnable = true;  // enable step 4 mode 1
-  bool bStarRefinementEnable = false;   // enable step 4 mode 2 (only one mode will be executed)
+  const int iSearchRange = 96;  // search range for each stage
+  const int iRaster = 5;  // search distance limit and downsampling factor for step 3                   
+  const unsigned step2_type = 0;  // search patterns for steps 2 and 4
+  const unsigned step4_type = 0;
+  const bool bRasterRefinementEnable = true;  // enable step 4 mode 1
+  const bool bStarRefinementEnable = false;   // enable step 4 mode 2 (only one mode will be executed)
   
-  int block_width = CU_WIDTH_FROM_DEPTH(depth);
+  const int block_width = CU_WIDTH_FROM_DEPTH(depth);
 
   vector2d_t mv = { mv_in_out->x >> 2, mv_in_out->y >> 2 };
 
