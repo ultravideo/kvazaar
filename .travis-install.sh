3a11e011eeab928b238aed4a8e69b4f7f9f3743a
Ari Koivula <ari@koivu.la>
2015-06-01 16:15:46 +0300

    Fix CI tests.
    - The ffmpeg version got bumped. To make sure this doesn't happen again,
      let's just host it our selves.

--- .travis-install.sh
+++ .travis-install.sh
@@ -13,11 +13,11 @@ fi
 if [ -n "$VALGRIND_TEST" ]; then
   sudo apt-get install -qq valgrind
   sudo apt-get install -qq p7zip-full
-  wget http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-32bit-static.tar.xz
+  wget http://ultravideo.cs.tut.fi/ffmpeg-release-32bit-static.tar.xz
   7z x ffmpeg-release-32bit-static.tar.xz
   7z x ffmpeg-release-32bit-static.tar
-  chmod +x ./ffmpeg-2.6.2-32bit-static/ffmpeg
-  ./ffmpeg-2.6.2-32bit-static/ffmpeg -f lavfi -i "mandelbrot=size=${TEST_DIM}:end_pts=10" -vframes $TEST_FRAMES -pix_fmt yuv420p mandelbrot_${TEST_DIM}.yuv
+  chmod +x ./ffmpeg-2.6.3-32bit-static/ffmpeg
+  ./ffmpeg-2.6.3-32bit-static/ffmpeg -f lavfi -i "mandelbrot=size=${TEST_DIM}:end_pts=10" -vframes $TEST_FRAMES -pix_fmt yuv420p mandelbrot_${TEST_DIM}.yuv
 fi
 
 if [ -n "$USE_YASM" ]; then
