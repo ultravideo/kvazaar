59f95b8e73c6e9a5d2e70037989c7851271001c3
Arttu Ylä-Outinen <arttu.yla-outinen@tut.fi>
2015-07-17 12:42:26 +0300

    Add nasm support.
    Makes is possible to build kvazaar using nasm instead of yasm.
    - Adds trailing slashes to -I params in ASFLAGS.
    - Disables CPU NOP directives when assembler is not yasm.

--- src/extras/x86inc.asm
+++ src/extras/x86inc.asm
@@ -82,8 +82,14 @@
     default rel
 %endif
 
+%macro CPUNOP 1
+    %ifdef __YASM_MAJOR__
+        CPU %1
+    %endif
+%endmacro
+
 ; Always use long nops (reduces 0x90 spam in disassembly on x86_32)
-CPU amdnop
+CPUNOP amdnop
 
 ; Macros to eliminate most code duplication between x86_32 and x86_64:
 ; Currently this works only for leaf functions which load all their arguments
@@ -745,7 +751,7 @@ SECTION .note.GNU-stack noalloc noexec nowrite progbits
 ; All subsequent functions (up to the next INIT_CPUFLAGS) is built for the specified cpu.
 ; You shouldn't need to invoke this macro directly, it's a subroutine for INIT_MMX &co.
 %macro INIT_CPUFLAGS 0-2
-    CPU amdnop
+    CPUNOP amdnop
     %if %0 >= 1
         %xdefine cpuname %1
         %assign cpuflags cpuflags_%1
@@ -768,7 +774,7 @@ SECTION .note.GNU-stack noalloc noexec nowrite progbits
             %define movu lddqu
         %endif
         %if ARCH_X86_64 == 0 && notcpuflag(sse2)
-            CPU basicnop
+            CPUNOP basicnop
         %endif
     %else
         %xdefine SUFFIX
