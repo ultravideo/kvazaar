beec2705b1d5013c8c1b0b04f64f996a0e5022ee
Ari Koivula <ari@koivu.la>
2015-07-31 15:51:53 +0300

    Add cli, lib-shared and lib-static to Makefile.

--- .travis-script.sh
+++ .travis-script.sh
@@ -7,7 +7,7 @@ if [ -n "$VALGRIND_TEST" ]; then
   valgrind --leak-check=full --error-exitcode=1 ./kvazaar_debug -i ../mandelbrot_${TEST_DIM}.yuv --input-res=${TEST_DIM} -o /dev/null $VALGRIND_TEST
 elif [ -n "$EXPECTED_STATUS" ]; then
   cd src
-  make
+  make cli
   set +e
   ./kvazaar $PARAMS
   EXIT_STATUS=$?
