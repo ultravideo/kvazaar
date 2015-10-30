6dce1f1e33cd049f29113905be3b8e809d1b8c14
Ari Lemmetti <ari.lemmetti@gmail.com>
2015-10-30 17:31:55 +0200

    Update versions for a new release

--- src/global.h
+++ src/global.h
@@ -144,7 +144,7 @@ typedef int16_t coeff_t;
 
 // NOTE: When making a release, remember to also bump library version in
 // Makefile, if necessary.
-#define KVZ_VERSION 0.7.1
+#define KVZ_VERSION 0.7.2
 #define VERSION_STRING QUOTE_EXPAND(KVZ_VERSION)
 
 //#define VERBOSE 1
