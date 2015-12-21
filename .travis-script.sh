f763e8bc4acf9f790a76a1447cc35c5f04c9644b
Arttu Ylä-Outinen <arttu.yla-outinen@tut.fi>
2015-12-21 14:59:09 +0200

    Update Travis tests to use autotools.

--- .travis-script.sh
+++ .travis-script.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 set -ev
 
+./autogen.sh
+./configure
+make
+
 if [ -n "$VALGRIND_TEST" ]; then
-  cd src
-  make debug
-  valgrind --leak-check=full --error-exitcode=1 ./kvazaar_debug -i ../mandelbrot_${TEST_DIM}.yuv --input-res=${TEST_DIM} -o /dev/null $VALGRIND_TEST
+  libtool execute valgrind --leak-check=full --error-exitcode=1 -- \
+    src/kvazaar -i mandelbrot_${TEST_DIM}.yuv --input-res=${TEST_DIM} -o /dev/null \
+    $VALGRIND_TEST
 elif [ -n "$EXPECTED_STATUS" ]; then
-  cd src
-  make cli
   set +e
-  ./kvazaar $PARAMS
+  libtool execute src/kvazaar $PARAMS
   EXIT_STATUS=$?
   set -e
   [ "$EXIT_STATUS" = "$EXPECTED_STATUS" ]
 else
-  cd src
-  make
-  make tests
+  make check
 fi
