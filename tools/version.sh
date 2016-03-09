c515796a211a0f03ab62f594d49562a8d92f5c77
Ricardo Constantino <wiiaboo@gmail.com>
2016-03-09 16:13:59 +0000

    Only use version prefix in kvazaar binary
    Fixes regression since 54f08f2 causing libkvazaar version checks to not
    work (i.e. pkg-config)

--- tools/version.sh
+++ tools/version.sh
@@ -6,7 +6,7 @@ cd ..
 if type git >/dev/null 2>/dev/null && [ -d .git ]; then
     version="$(git describe --dirty --tags --match 'v*')"
 else
-    version="$(awk '/#define KVZ_VERSION/ { print $3 }' src/global.h)"
+    version="v$(awk '/#define KVZ_VERSION/ { print $3 }' src/global.h)"
 fi
 
 printf '%s\n' "$version"
