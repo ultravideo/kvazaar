7947b186493e07843ef0f770cfe995497433df14
Ari Koivula <ari@koivu.la>
2016-05-31 13:35:20 +0300

    Add bitstream check to valgrind tests

--- .travis-script.sh
+++ .travis-script.sh
@@ -7,8 +7,9 @@ make --jobs=2 V=1
 
 if [ -n "$VALGRIND_TEST" ]; then
   libtool execute valgrind --leak-check=full --error-exitcode=1 -- \
-    src/kvazaar -i mandelbrot_${TEST_DIM}.yuv --input-res=${TEST_DIM} -o /dev/null \
-    $VALGRIND_TEST
+    src/kvazaar -i mandelbrot_${TEST_DIM}.yuv --input-res=${TEST_DIM} \
+    -o test.265 $VALGRIND_TEST
+    ./hmdec-16.10 -b test.265
 elif [ -n "$EXPECTED_STATUS" ]; then
   set +e
   libtool execute src/kvazaar $PARAMS
