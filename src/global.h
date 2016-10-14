1dcc9937439422c65ca3bf46d229f52ce3fe1bb9
Alexis Ballier <aballier@gentoo.org>
2016-10-14 18:06:37 +0200

    Include i386 & i486 for compiling intel asm.
    x86_64-pc-linux-gnu-gcc -m32 that I use for building 32bits libraries on amd64 defines only __i386__.

--- src/global.h
+++ src/global.h
@@ -253,7 +253,7 @@ typedef enum { COLOR_Y = 0, COLOR_U, COLOR_V } color_t;
 
 
 // Hardware data (abstraction of defines). Extend for other compilers
-#if defined(_M_IX86) || defined(__i586__) || defined(__i686__) || defined(_M_X64) || defined(_M_AMD64) || defined(__amd64__) || defined(__x86_64__)
+#if defined(_M_IX86) || defined(__i386__) || defined(__i486__) || defined(__i586__) || defined(__i686__) || defined(_M_X64) || defined(_M_AMD64) || defined(__amd64__) || defined(__x86_64__)
 #  define COMPILE_INTEL 1
 #else
 #  define COMPILE_INTEL 0
