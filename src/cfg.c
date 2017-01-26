e1b36c8ee3502fae40bcadbe7f0301aeca2fcdd4
Jaakko Laitinen <jaakko.laitinen@tut.fi>
2017-01-26 09:10:35 +0200

    Add static keyword to const arrays

--- src/cfg.c
+++ src/cfg.c
@@ -861,8 +861,8 @@ int kvz_config_parse(kvz_config *cfg, const char *name, const char *value)
       cfg->max_temporal_layer = 4;
       
       //tIds for 7 max_layers. Can be used for smaller max_layers ( tId = tIds[i]-7+max_temporal_layers-1, i!=0 )
-      const uint8_t tIds[] = {0, 6, 5, 6, 4, 6, 5, 6, 3, 6, 5, 6, 4, 6, 5, 6, 2, 6, 5, 6, 4, 6, 5, 6, 3, 6, 5, 6, 4, 6, 5, 6,
-                              1, 6, 5, 6, 4, 6, 5, 6, 3, 6, 5, 6, 4, 6, 5, 6, 2, 6, 5, 6, 4, 6, 5, 6, 3, 6, 5, 6, 4, 6, 5, 6};
+      static const uint8_t tIds[] = {0, 6, 5, 6, 4, 6, 5, 6, 3, 6, 5, 6, 4, 6, 5, 6, 2, 6, 5, 6, 4, 6, 5, 6, 3, 6, 5, 6, 4, 6, 5, 6,
+                                     1, 6, 5, 6, 4, 6, 5, 6, 3, 6, 5, 6, 4, 6, 5, 6, 2, 6, 5, 6, 4, 6, 5, 6, 3, 6, 5, 6, 4, 6, 5, 6};
 
       for (int i = 0; i < cfg->gop_len; i++) {
         uint8_t t_offset = cfg->gop[i].poc_offset%cfg->gop_len == 0 ? 0 : cfg->max_temporal_layer - 7;
@@ -1169,8 +1169,8 @@ void kvz_config_process_lp_gop(kvz_config *cfg)
   //Set tIds
   //tIds for 7 max_layers. Can be used for smaller max_layers ( tId = tIds[i]-7+max_temporal_layers, i!=0 ) 
   if (cfg->max_temporal_layer > 1) {
-    const uint8_t tIds[] = {0, 6, 5, 6, 4, 6, 5, 6, 3, 6, 5, 6, 4, 6, 5, 6, 2, 6, 5, 6, 4, 6, 5, 6, 3, 6, 5, 6, 4, 6, 5, 6,
-                            1, 6, 5, 6, 4, 6, 5, 6, 3, 6, 5, 6, 4, 6, 5, 6, 2, 6, 5, 6, 4, 6, 5, 6, 3, 6, 5, 6, 4, 6, 5, 6};
+    static const uint8_t tIds[] = {0, 6, 5, 6, 4, 6, 5, 6, 3, 6, 5, 6, 4, 6, 5, 6, 2, 6, 5, 6, 4, 6, 5, 6, 3, 6, 5, 6, 4, 6, 5, 6,
+                                   1, 6, 5, 6, 4, 6, 5, 6, 3, 6, 5, 6, 4, 6, 5, 6, 2, 6, 5, 6, 4, 6, 5, 6, 3, 6, 5, 6, 4, 6, 5, 6};
 
     for (int i = 0; i < cfg->gop_len; i++) {
       uint8_t t_offset = cfg->gop[i].poc_offset%cfg->gop_len == 0 ? 0 : cfg->max_temporal_layer - 7;
