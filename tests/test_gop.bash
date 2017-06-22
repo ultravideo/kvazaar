3da5558956e02e16377d256c155f24c1b6dc489a
Arttu Ylä-Outinen <arttu.yla-outinen@tut.fi>
2017-06-22 09:44:01 +0300

    Add an LP-GOP test

--- tests/test_gop.bash
+++ tests/test_gop.bash
@@ -5,7 +5,8 @@
 set -eu
 source util.bash
 
-common_args='--gop=8 -p0 --threads=2 --wpp --rd=0 --no-rdoq --no-deblock --no-sao --no-signhide --subme=0 --pu-depth-inter=1-3 --pu-depth-intra=2-3'
-valgrind_test 264x130 10 $common_args --owf=1
-valgrind_test 264x130 10 $common_args --owf=4
-valgrind_test 264x130 20 $common_args --owf=0
+common_args='-p0 --threads=2 --wpp --rd=0 --no-rdoq --no-deblock --no-sao --no-signhide --subme=0 --pu-depth-inter=1-3 --pu-depth-intra=2-3'
+valgrind_test 264x130 10 $common_args --gop=8 -p0 --owf=1
+valgrind_test 264x130 10 $common_args --gop=8 -p0 --owf=4
+valgrind_test 264x130 20 $common_args --gop=8 -p16 --owf=0
+valgrind_test 264x130 10 $common_args --gop=lp-g4d3t1 -p5 --owf=4
