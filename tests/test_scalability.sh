740b7e6dae2db502f15f2cf51cf0e707174746bd
Jaakko Laitinen <jaakko.laitinen@tut.fi>
2017-09-20 13:45:03 +0300

    Remove --no-wpp from test

--- tests/test_scalability.sh
+++ tests/test_scalability.sh
@@ -6,8 +6,8 @@ set -eu
 . "${0%/*}/util.sh"
 
 # Basic scalable encoding
-valgrind_test 512x264 20 --preset=ultrafast -p12 --layer-res=256x132 -q30 -r3 --gop=0 --layer --preset=ultrafast -q28 -r0 --ilr=1 --gop=0 --no-wpp
-valgrind_test 512x264 20 --preset=ultrafast -p12 --layer-res=256x132 -q30 -r3 --gop=0 --layer --preset=ultrafast -q28 -r2 --ilr=1 --gop=0 --no-wpp
+valgrind_test 512x264 20 --preset=ultrafast -p12 --layer-res=256x132 -q30 -r3 --gop=0 --layer --preset=ultrafast -q28 -r0 --ilr=1 --gop=0
+valgrind_test 512x264 20 --preset=ultrafast -p12 --layer-res=256x132 -q30 -r3 --gop=0 --layer --preset=ultrafast -q28 -r2 --ilr=1 --gop=0
 
 # Test encoder control initing failures when using layers. TODO: strictly necessary?
 valgrind_expected_test 264x130 10 1 --ilr=1
