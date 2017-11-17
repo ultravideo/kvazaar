cf85d52b9d953d29bcc9f7e158a6a2be6f310078
Arttu Ylä-Outinen <arttu.yla-outinen@tut.fi>
2017-11-17 15:23:33 +0200

    Kvazaar version 1.2.0

--- src/global.h
+++ src/global.h
@@ -200,7 +200,7 @@ typedef int16_t coeff_t;
 // NOTE: When making a release, check to see if incrementing libversion in 
 // configure.ac is necessary.
 #ifndef KVZ_VERSION
-#define KVZ_VERSION 1.1.0
+#define KVZ_VERSION 1.2.0
 #endif
 #define VERSION_STRING QUOTE_EXPAND(KVZ_VERSION)
 
