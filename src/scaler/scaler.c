c5568ca813b6bee3aef53bd274719b4fefddfe0b
Jaakko Laitinen <jaakko.laitinen@tut.fi>
2018-07-10 14:15:40 +0300

    Use 1D arrays to remove the need for heavy filter selection function use in the main loop of resampling

--- src/scaler/scaler.c
+++ src/scaler/scaler.c
@@ -36,7 +36,7 @@
 
 //Define filters for scaling operations
 //Values from SHM
-static const int filter16[8][16][12] = {
+static const int downFilter[8][16][12] = {
   // ratio <= 20/19
   {
     {0, 0, 0, 0, 0, 128, 0, 0, 0, 0, 0, 0},
@@ -227,6 +227,199 @@ static const int chromaUpFilter[16][4] = {
   {-2, 14, 56, -4},
   {-2, 10, 58, -2},
   {0, 4, 62, -2}
+};
+
+static const int downFilter1D[8][16*12] = {
+  // ratio <= 20/19
+  {
+     0, 0, 0, 0, 0, 128, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 2, -6, 127, 7, -2, 0, 0, 0, 0,
+     0, 0, 0, 3, -12, 125, 16, -5, 1, 0, 0, 0,
+     0, 0, 0, 4, -16, 120, 26, -7, 1, 0, 0, 0,
+     0, 0, 0, 5, -18, 114, 36, -10, 1, 0, 0, 0,
+     0, 0, 0, 5, -20, 107, 46, -12, 2, 0, 0, 0,
+     0, 0, 0, 5, -21, 99, 57, -15, 3, 0, 0, 0,
+     0, 0, 0, 5, -20, 89, 68, -18, 4, 0, 0, 0,
+     0, 0, 0, 4, -19, 79, 79, -19, 4, 0, 0, 0,
+     0, 0, 0, 4, -18, 68, 89, -20, 5, 0, 0, 0,
+     0, 0, 0, 3, -15, 57, 99, -21, 5, 0, 0, 0,
+     0, 0, 0, 2, -12, 46, 107, -20, 5, 0, 0, 0,
+     0, 0, 0, 1, -10, 36, 114, -18, 5, 0, 0, 0,
+     0, 0, 0, 1, -7, 26, 120, -16, 4, 0, 0, 0,
+     0, 0, 0, 1, -5, 16, 125, -12, 3, 0, 0, 0,
+     0, 0, 0, 0, -2, 7, 127, -6, 2, 0, 0, 0,
+  },
+  // 20/19 < ratio <= 5/4
+  {
+     0, 2, 0, -14, 33, 86, 33, -14, 0, 2, 0, 0,
+     0, 1, 1, -14, 29, 85, 38, -13, -1, 2, 0, 0,
+     0, 1, 2, -14, 24, 84, 43, -12, -2, 2, 0, 0,
+     0, 1, 2, -13, 19, 83, 48, -11, -3, 2, 0, 0,
+     0, 0, 3, -13, 15, 81, 53, -10, -4, 3, 0, 0,
+     0, 0, 3, -12, 11, 79, 57, -8, -5, 3, 0, 0,
+     0, 0, 3, -11, 7, 76, 62, -5, -7, 3, 0, 0,
+     0, 0, 3, -10, 3, 73, 65, -2, -7, 3, 0, 0,
+     0, 0, 3, -9, 0, 70, 70, 0, -9, 3, 0, 0,
+     0, 0, 3, -7, -2, 65, 73, 3, -10, 3, 0, 0,
+     0, 0, 3, -7, -5, 62, 76, 7, -11, 3, 0, 0,
+     0, 0, 3, -5, -8, 57, 79, 11, -12, 3, 0, 0,
+     0, 0, 3, -4, -10, 53, 81, 15, -13, 3, 0, 0,
+     0, 0, 2, -3, -11, 48, 83, 19, -13, 2, 1, 0,
+     0, 0, 2, -2, -12, 43, 84, 24, -14, 2, 1, 0,
+     0, 0, 2, -1, -13, 38, 85, 29, -14, 1, 1, 0,
+  },
+  // 5/4 < ratio <= 5/3
+  {
+     0, 5, -6, -10, 37, 76, 37, -10, -6, 5, 0, 0,
+     0, 5, -4, -11, 33, 76, 40, -9, -7, 5, 0, 0,
+     -1, 5, -3, -12, 29, 75, 45, -7, -8, 5, 0, 0,
+     -1, 4, -2, -13, 25, 75, 48, -5, -9, 5, 1, 0,
+     -1, 4, -1, -13, 22, 73, 52, -3, -10, 4, 1, 0,
+     -1, 4, 0, -13, 18, 72, 55, -1, -11, 4, 2, -1,
+     -1, 4, 1, -13, 14, 70, 59, 2, -12, 3, 2, -1,
+     -1, 3, 1, -13, 11, 68, 62, 5, -12, 3, 2, -1,
+     -1, 3, 2, -13, 8, 65, 65, 8, -13, 2, 3, -1,
+     -1, 2, 3, -12, 5, 62, 68, 11, -13, 1, 3, -1,
+     -1, 2, 3, -12, 2, 59, 70, 14, -13, 1, 4, -1,
+     -1, 2, 4, -11, -1, 55, 72, 18, -13, 0, 4, -1,
+     0, 1, 4, -10, -3, 52, 73, 22, -13, -1, 4, -1,
+     0, 1, 5, -9, -5, 48, 75, 25, -13, -2, 4, -1,
+     0, 0, 5, -8, -7, 45, 75, 29, -12, -3, 5, -1,
+     0, 0, 5, -7, -9, 40, 76, 33, -11, -4, 5, 0,
+  },
+  // 5/3 < ratio <= 2
+  {
+     2, -3, -9, 6, 39, 58, 39, 6, -9, -3, 2, 0,
+     2, -3, -9, 4, 38, 58, 43, 7, -9, -4, 1, 0,
+     2, -2, -9, 2, 35, 58, 44, 9, -8, -4, 1, 0,
+     1, -2, -9, 1, 34, 58, 46, 11, -8, -5, 1, 0,
+     1, -1, -8, -1, 31, 57, 47, 13, -7, -5, 1, 0,
+     1, -1, -8, -2, 29, 56, 49, 15, -7, -6, 1, 1,
+     1, 0, -8, -3, 26, 55, 51, 17, -7, -6, 1, 1,
+     1, 0, -7, -4, 24, 54, 52, 19, -6, -7, 1, 1,
+     1, 0, -7, -5, 22, 53, 53, 22, -5, -7, 0, 1,
+     1, 1, -7, -6, 19, 52, 54, 24, -4, -7, 0, 1,
+     1, 1, -6, -7, 17, 51, 55, 26, -3, -8, 0, 1,
+     1, 1, -6, -7, 15, 49, 56, 29, -2, -8, -1, 1,
+     0, 1, -5, -7, 13, 47, 57, 31, -1, -8, -1, 1,
+     0, 1, -5, -8, 11, 46, 58, 34, 1, -9, -2, 1,
+     0, 1, -4, -8, 9, 44, 58, 35, 2, -9, -2, 2,
+     0, 1, -4, -9, 7, 43, 58, 38, 4, -9, -3, 2,
+  },
+  // 2 < ratio <= 5/2
+  {
+     -2, -7, 0, 17, 35, 43, 35, 17, 0, -7, -5, 2,
+     -2, -7, -1, 16, 34, 43, 36, 18, 1, -7, -5, 2,
+     -1, -7, -1, 14, 33, 43, 36, 19, 1, -6, -5, 2,
+     -1, -7, -2, 13, 32, 42, 37, 20, 3, -6, -5, 2,
+     0, -7, -3, 12, 31, 42, 38, 21, 3, -6, -5, 2,
+     0, -7, -3, 11, 30, 42, 39, 23, 4, -6, -6, 1,
+     0, -7, -4, 10, 29, 42, 40, 24, 5, -6, -6, 1,
+     1, -7, -4, 9, 27, 41, 40, 25, 6, -5, -6, 1,
+     1, -6, -5, 7, 26, 41, 41, 26, 7, -5, -6, 1,
+     1, -6, -5, 6, 25, 40, 41, 27, 9, -4, -7, 1,
+     1, -6, -6, 5, 24, 40, 42, 29, 10, -4, -7, 0,
+     1, -6, -6, 4, 23, 39, 42, 30, 11, -3, -7, 0,
+     2, -5, -6, 3, 21, 38, 42, 31, 12, -3, -7, 0,
+     2, -5, -6, 3, 20, 37, 42, 32, 13, -2, -7, -1,
+     2, -5, -6, 1, 19, 36, 43, 33, 14, -1, -7, -1,
+     2, -5, -7, 1, 18, 36, 43, 34, 16, -1, -7, -2,
+  },
+  // 5/2 < ratio <= 20/7
+  {
+     -6, -3, 5, 19, 31, 36, 31, 19, 5, -3, -6, 0,
+     -6, -4, 4, 18, 31, 37, 32, 20, 6, -3, -6, -1,
+     -6, -4, 4, 17, 30, 36, 33, 21, 7, -3, -6, -1,
+     -5, -5, 3, 16, 30, 36, 33, 22, 8, -2, -6, -2,
+     -5, -5, 2, 15, 29, 36, 34, 23, 9, -2, -6, -2,
+     -5, -5, 2, 15, 28, 36, 34, 24, 10, -2, -6, -3,
+     -4, -5, 1, 14, 27, 36, 35, 24, 10, -1, -6, -3,
+     -4, -5, 0, 13, 26, 35, 35, 25, 11, 0, -5, -3,
+     -4, -6, 0, 12, 26, 36, 36, 26, 12, 0, -6, -4,
+     -3, -5, 0, 11, 25, 35, 35, 26, 13, 0, -5, -4,
+     -3, -6, -1, 10, 24, 35, 36, 27, 14, 1, -5, -4,
+     -3, -6, -2, 10, 24, 34, 36, 28, 15, 2, -5, -5,
+     -2, -6, -2, 9, 23, 34, 36, 29, 15, 2, -5, -5,
+     -2, -6, -2, 8, 22, 33, 36, 30, 16, 3, -5, -5,
+     -1, -6, -3, 7, 21, 33, 36, 30, 17, 4, -4, -6,
+     -1, -6, -3, 6, 20, 32, 37, 31, 18, 4, -4, -6,
+  },
+  // 20/7 < ratio <= 15/4
+  {
+     -9, 0, 9, 20, 28, 32, 28, 20, 9, 0, -9, 0,
+     -9, 0, 8, 19, 28, 32, 29, 20, 10, 0, -4, -5,
+     -9, -1, 8, 18, 28, 32, 29, 21, 10, 1, -4, -5,
+     -9, -1, 7, 18, 27, 32, 30, 22, 11, 1, -4, -6,
+     -8, -2, 6, 17, 27, 32, 30, 22, 12, 2, -4, -6,
+     -8, -2, 6, 16, 26, 32, 31, 23, 12, 2, -4, -6,
+     -8, -2, 5, 16, 26, 31, 31, 23, 13, 3, -3, -7,
+     -8, -3, 5, 15, 25, 31, 31, 24, 14, 4, -3, -7,
+     -7, -3, 4, 14, 25, 31, 31, 25, 14, 4, -3, -7,
+     -7, -3, 4, 14, 24, 31, 31, 25, 15, 5, -3, -8,
+     -7, -3, 3, 13, 23, 31, 31, 26, 16, 5, -2, -8,
+     -6, -4, 2, 12, 23, 31, 32, 26, 16, 6, -2, -8,
+     -6, -4, 2, 12, 22, 30, 32, 27, 17, 6, -2, -8,
+     -6, -4, 1, 11, 22, 30, 32, 27, 18, 7, -1, -9,
+     -5, -4, 1, 10, 21, 29, 32, 28, 18, 8, -1, -9,
+     -5, -4, 0, 10, 20, 29, 32, 28, 19, 8, 0, -9,
+  },
+  // ratio > 15/4
+  {
+     -8, 7, 13, 18, 22, 24, 22, 18, 13, 7, 2, -10,
+     -8, 7, 13, 18, 22, 23, 22, 19, 13, 7, 2, -10,
+     -8, 6, 12, 18, 22, 23, 22, 19, 14, 8, 2, -10,
+     -9, 6, 12, 17, 22, 23, 23, 19, 14, 8, 3, -10,
+     -9, 6, 12, 17, 21, 23, 23, 19, 14, 9, 3, -10,
+     -9, 5, 11, 17, 21, 23, 23, 20, 15, 9, 3, -10,
+     -9, 5, 11, 16, 21, 23, 23, 20, 15, 9, 4, -10,
+     -9, 5, 10, 16, 21, 23, 23, 20, 15, 10, 4, -10,
+     -10, 5, 10, 16, 20, 23, 23, 20, 16, 10, 5, -10,
+     -10, 4, 10, 15, 20, 23, 23, 21, 16, 10, 5, -9,
+     -10, 4, 9, 15, 20, 23, 23, 21, 16, 11, 5, -9,
+     -10, 3, 9, 15, 20, 23, 23, 21, 17, 11, 5, -9,
+     -10, 3, 9, 14, 19, 23, 23, 21, 17, 12, 6, -9,
+     -10, 3, 8, 14, 19, 23, 23, 22, 17, 12, 6, -9,
+     -10, 2, 8, 14, 19, 22, 23, 22, 18, 12, 6, -8,
+     -10, 2, 7, 13, 19, 22, 23, 22, 18, 13, 7, -8,
+  }
+};
+
+static const int lumaUpFilter1D[16*8] = {
+    0, 0, 0, 64, 0, 0, 0, 0,
+    0, 1, -3, 63, 4, -2, 1, 0,
+    -1, 2, -5, 62, 8, -3, 1, 0,
+    -1, 3, -8, 60, 13, -4, 1, 0,
+    -1, 4, -10, 58, 17, -5, 1, 0,
+    -1, 4, -11, 52, 26, -8, 3, -1,
+    -1, 3, -9, 47, 31, -10, 4, -1,
+    -1, 4, -11, 45, 34, -10, 4, -1,
+    -1, 4, -11, 40, 40, -11, 4, -1,
+    -1, 4, -10, 34, 45, -11, 4, -1,
+    -1, 4, -10, 31, 47, -9, 3, -1,
+    -1, 3, -8, 26, 52, -11, 4, -1,
+    0, 1, -5, 17, 58, -10, 4, -1,
+    0, 1, -4, 13, 60, -8, 3, -1,
+    0, 1, -3, 8, 62, -5, 2, -1,
+    0, 1, -2, 4, 63, -3, 1, 0
+};
+
+static const int chromaUpFilter1D[16*4] = {
+    0, 64, 0, 0,
+    -2, 62, 4, 0,
+    -2, 58, 10, -2,
+    -4, 56, 14, -2,
+    -4, 54, 16, -2,
+    -6, 52, 20, -2,
+    -6, 46, 28, -4,
+    -4, 42, 30, -4,
+    -4, 36, 36, -4,
+    -4, 30, 42, -4,
+    -4, 28, 46, -6,
+    -2, 20, 52, -6,
+    -2, 16, 54, -4,
+    -2, 14, 56, -4,
+    -2, 10, 58, -2,
+    0, 4, 62, -2 
 };
 
 //Used for clipping values
@@ -720,9 +913,31 @@ static int getFilter(const int** const filter, int is_upsampling, int is_luma, i
   }
 
   //Downsampling so use 12-tap filter
-  *filter = filter16[filter_ind][phase];
-  return (sizeof(filter16[0][0]) / sizeof(filter16[0][0][0]));
+  *filter = downFilter[filter_ind][phase];
+  return (sizeof(downFilter[0][0]) / sizeof(downFilter[0][0][0]));
 }
+
+//Helper function for choosing the correct filter
+//Returns the size of the filter and the filter param is set to the correct filter
+static int prepareFilter(const int** const filter, int is_upsampling, int is_luma, int filter_ind)
+{
+  if (is_upsampling) {
+    //Upsampling so use 8- or 4-tap filters
+    if (is_luma) {
+      *filter = lumaUpFilter1D;
+      return sizeof(lumaUpFilter[0]) / sizeof(lumaUpFilter[0][0]);
+    }
+
+    *filter = chromaUpFilter1D;
+    return sizeof(chromaUpFilter[0]) / sizeof(chromaUpFilter[0][0]);
+  }
+
+  //Downsampling so use 12-tap filter
+  *filter = downFilter1D[filter_ind];
+  return (sizeof(downFilter[0][0]) / sizeof(downFilter[0][0][0]));
+}
+
+#define getFilterCoeff(filter, stride, phase, ind) ((filter)[(ind)+(phase)*(stride)])
 
 //Resampling is done here per buffer
 static void _resample(const pic_buffer_t* const buffer, const scaling_parameter_t* const param, const int is_upscaling, const int is_luma)
@@ -888,6 +1103,11 @@ static void resample(const pic_buffer_t* const buffer, const scaling_parameter_t
       hor_filter = 1;
   }
 
+  const int *filter_hor;
+  const int filter_size_hor = prepareFilter(&filter_hor, is_upscaling, is_luma, hor_filter);
+  const int *filter_ver;
+  const int filter_size_ver = prepareFilter(&filter_ver, is_upscaling, is_luma, ver_filter);
+
   int shift_x = param->shift_x - 4;
   int shift_y = param->shift_y - 4;
 
@@ -905,7 +1125,9 @@ static void resample(const pic_buffer_t* const buffer, const scaling_parameter_t
 
       //Choose filter
       const int* filter;
-      int size = getFilter(&filter, is_upscaling, is_luma, phase, hor_filter);
+      //int size = getFilter(&filter, is_upscaling, is_luma, phase, hor_filter);
+      int size = filter_size_hor;
+      filter = &getFilterCoeff(filter_hor, size, phase, 0);
 
       //Apply filter
       tmp_row[j] = 0;
@@ -931,7 +1153,9 @@ static void resample(const pic_buffer_t* const buffer, const scaling_parameter_t
 
       //Choose filter
       const int* filter;
-      int size = getFilter(&filter, is_upscaling, is_luma, phase, ver_filter);
+      //int size = getFilter(&filter, is_upscaling, is_luma, phase, ver_filter);
+      int size = filter_size_ver;
+      filter = &getFilterCoeff(filter_ver, size, phase, 0);
 
       //Apply filter
       tmp_col[j] = 0;
@@ -987,8 +1211,8 @@ static void resampleBlockStep(const pic_buffer_t* const src_buffer, const pic_bu
   const int delta = is_vertical ? param->delta_y : param->delta_x;
 
   //Set loop parameters based on the resampling dir
-  const int *filt;
-  const int filter_size = getFilter(&filt, is_upscaling, is_luma, 0, 0);
+  const int *filter;
+  const int filter_size = prepareFilter(&filter, is_upscaling, is_luma, filter_phase);
   const int outer_init = is_vertical ? 0 : block_x;
   const int outer_bound = is_vertical ? filter_size : block_x + block_width;
   const int inner_init = is_vertical ? block_x : 0;
@@ -1022,19 +1246,19 @@ static void resampleBlockStep(const pic_buffer_t* const src_buffer, const pic_bu
         int ref_pos = ref_pos_16 >> 4;
 
         //Choose filter
-        const int *filter;
-        const int f_size = getFilter(&filter, is_upscaling, is_luma, phase, filter_phase);
+        //const int *filter;
+        //const int f_size = getFilter(&filter, is_upscaling, is_luma, phase, filter_phase);
 
         //Set trgt buffer val to zero on first loop over filter
         if( f_ind == 0 ){
           trgt_row[t_col + trgt_offset] = 0;
         }
 
-        const int s_ind = clip(ref_pos + f_ind - (f_size >> 1) + 1, 0, src_size - 1); //src_buffer row/col index for cur resampling dir
+        const int s_ind = clip(ref_pos + f_ind - (filter_size >> 1) + 1, 0, src_size - 1); //src_buffer row/col index for cur resampling dir
 
         //Move src pointer to correct position (correct column in vertical resampling)
         pic_data_t *src_col = src + (is_vertical ? i_ind : 0);
-        trgt_row[t_col + trgt_offset] += filter[f_ind] * src_col[s_ind * s_stride + src_offset];
+        trgt_row[t_col + trgt_offset] += getFilterCoeff(filter,filter_size,phase,f_ind) * src_col[s_ind * s_stride + src_offset];
 
         //Scale values in trgt buffer to the correct range. Only done in the final loop over o_ind (block width)
         if (is_vertical && o_ind == outer_bound - 1) {
@@ -1674,7 +1898,7 @@ static void blockScalingSrcRange( int range[2], const int scale, const int add, 
   }
 
   //Get filter size
-  int size = scale < SCALER_UNITY_SCALE_CONST ? sizeof(lumaUpFilter[0]) / sizeof(lumaUpFilter[0][0]) : sizeof(filter16[0][0]) / sizeof(filter16[0][0][0]);
+  int size = scale < SCALER_UNITY_SCALE_CONST ? sizeof(lumaUpFilter[0]) / sizeof(lumaUpFilter[0][0]) : sizeof(downFilter[0][0]) / sizeof(downFilter[0][0][0]);
 
   //Calculate lower bound
   range[0] = ((int)((unsigned int)((block_low * scale + add) >> (shift - 4)) - delta) >> 4) - (size >> 1) + 1;
