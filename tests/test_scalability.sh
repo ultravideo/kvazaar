bad11bdb9435d858c477e2b84df648598e7c8a7a
Jaakko Laitinen <jaakko.laitinen@tut.fi>
2018-08-30 15:07:44 +0300

    Set valid intra periods for B-gop scalability tests

--- tests/test_scalability.sh
+++ tests/test_scalability.sh
@@ -27,11 +27,11 @@ valgrind_test 512x264 20 --preset=ultrafast -p12 -q30 -r3 --gop=0 --layer --pres
 
 # Test with GOP
 valgrind_test 512x264 20 --preset=ultrafast -p12 --layer-res=256x132 -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1
-valgrind_test 512x264 20 --preset=ultrafast -p12 --layer-res=256x132 -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1 --gop=8
+valgrind_test 512x264 20 --preset=ultrafast -p16 --layer-res=256x132 -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1 --gop=8
 
 #   Test without threads
 valgrind_test 512x264 20 --preset=ultrafast -p12 --layer-res=256x132 -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1 --threads=0 --owf=0
-valgrind_test 512x264 20 --preset=ultrafast -p12 --layer-res=256x132 -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1 --gop=8 --threads=0 --owf=0
+valgrind_test 512x264 20 --preset=ultrafast -p16 --layer-res=256x132 -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1 --gop=8 --threads=0 --owf=0
 
 # Test encoder control initing failures when using layers. TODO: strictly necessary?
 valgrind_expected_test 264x130 10 1 --ilr=1
