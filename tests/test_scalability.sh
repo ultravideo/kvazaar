cd1af5a896b6a9ede81424945019c61911b17e14
Jaakko Laitinen <jaakko.laitinen@tut.fi>
2018-11-23 15:14:55 +0200

    Add SAO deblock tests

--- tests/test_scalability.sh
+++ tests/test_scalability.sh
@@ -33,6 +33,21 @@ valgrind_test 512x264 20 --preset=ultrafast -p16 --layer-res=256x132 -q30 -r3 --
 valgrind_test 512x264 20 --preset=ultrafast -p12 --layer-res=256x132 -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1 --threads=0 --owf=0
 valgrind_test 512x264 20 --preset=ultrafast -p16 --layer-res=256x132 -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1 --gop=8 --threads=0 --owf=0
 
+# Test SAO/deblock
+valgrind_test 512x264 20 --preset=ultrafast -p12  --no-deblock --sao=full -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1  --sao=off
+valgrind_test 512x264 20 --preset=ultrafast -p12 --layer-res=256x132 --no-deblock --sao=full -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1  --sao=off
+valgrind_test 512x264 20 --preset=ultrafast -p12  --no-deblock --sao=full -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1  --sao=full
+valgrind_test 512x264 20 --preset=ultrafast -p12 --layer-res=256x132 --no-deblock --sao=full -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1  --sao=full
+valgrind_test 512x264 20 --preset=ultrafast -p12   --sao=full -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1 --no-deblock --sao=off
+valgrind_test 512x264 20 --preset=ultrafast -p12 --layer-res=256x132  --sao=full -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1 --no-deblock --sao=off
+valgrind_test 512x264 20 --preset=ultrafast -p12   --sao=full -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1 --no-deblock --sao=full
+valgrind_test 512x264 20 --preset=ultrafast -p12 --layer-res=256x132  --sao=full -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1 --no-deblock --sao=full
+valgrind_test 512x264 20 --preset=ultrafast -p12  --no-deblock --sao=full -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1 --no-deblock --sao=off
+valgrind_test 512x264 20 --preset=ultrafast -p12 --layer-res=256x132 --no-deblock --sao=full -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1 --no-deblock --sao=off
+valgrind_test 512x264 20 --preset=ultrafast -p12  --no-deblock --sao=full -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1 --no-deblock --sao=full
+valgrind_test 512x264 20 --preset=ultrafast -p12 --layer-res=256x132 --no-deblock --sao=full -q30 -r3 --layer --preset=ultrafast -q28 -r2 --ilr=1 --no-deblock --sao=full
+
+
 # Test encoder control initing failures when using layers. TODO: strictly necessary?
 valgrind_expected_test 264x130 10 1 --ilr=1
 valgrind_expected_test 264x130 10 1 --no-wpp --gop=0 --layer --ilr=2
