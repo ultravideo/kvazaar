41f51c08c4dc1db1bf69ea3811620a1711823791
Pauli Oikkonen <pauli.oikkonen@tut.fi>
2019-03-01 15:37:06 +0200

    Avoid overrunning buffer in hor_sad_sse41_w32

--- src/strategies/sse41/picture-sse41.c
+++ src/strategies/sse41/picture-sse41.c
@@ -87,53 +87,79 @@ static uint32_t hor_sad_sse41_w32(const kvz_pixel *pic_data, const kvz_pixel *re
                                   int32_t height, uint32_t pic_stride, uint32_t ref_stride,
                                   uint32_t left, uint32_t right)
 {
+  const size_t vec_width       = 16;
+  const uint32_t blkwidth_log2 = 5;
+  const uint32_t left_eq_wid   = left  >> blkwidth_log2;
+  const uint32_t right_eq_wid  = right >> blkwidth_log2;
+  const int32_t  left_clamped  = left  - left_eq_wid;
+  const int32_t  right_clamped = right - right_eq_wid;
+
   const int32_t height_twoline_groups = height & ~1;
   const int32_t height_residual_lines = height &  1;
 
-  __m128i nslo = _mm_setr_epi8(0,  1,  2,  3,  4,  5,  6,  7,
-                               8,  9,  10, 11, 12, 13, 14, 15);
-  __m128i nshi = _mm_setr_epi8(16, 17, 18, 19, 20, 21, 22, 23,
-                               24, 25, 26, 27, 28, 29, 30, 31);
-  __m128i epol_masklo, epol_maskhi;
-  int32_t border_pix_off;
+  const __m128i zero       = _mm_setzero_si128();
+  const __m128i vec_widths = _mm_set1_epi8((uint8_t)vec_width);
+  const __m128i lefts      = _mm_set1_epi8((uint8_t)left_clamped);
+  const __m128i rights     = _mm_set1_epi8((uint8_t)right_clamped);
+  const __m128i nslo       = _mm_setr_epi8(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+  const __m128i nshi       = _mm_add_epi8 (nslo, vec_widths);
 
-  if (left) {
-    border_pix_off          = left;
-    __m128i first_valid_idx = _mm_set1_epi8(left);
+  const __m128i rightmost_good_idx = _mm_set1_epi8((uint8_t)((vec_width << 1) - right - 1));
 
-    epol_masklo = _mm_cmpgt_epi8(first_valid_idx, nslo);
-    epol_maskhi = _mm_cmpgt_epi8(first_valid_idx, nshi);
-  } else {
-    border_pix_off          = 31 - right;
-    __m128i last_valid_idx  = _mm_set1_epi8(border_pix_off);
+  const __m128i epol_mask_right_lo = _mm_min_epi8  (nslo,            rightmost_good_idx);
+  const __m128i epol_mask_right_hi = _mm_min_epi8  (nshi,            rightmost_good_idx);
+  const __m128i epol_mask_lo       = _mm_max_epi8  (lefts,           epol_mask_right_lo);
+  const __m128i epol_mask_hi       = _mm_max_epi8  (lefts,           epol_mask_right_hi);
 
-    epol_masklo = _mm_cmpgt_epi8(nslo, last_valid_idx);
-    epol_maskhi = _mm_cmpgt_epi8(nshi, last_valid_idx);
-  }
+  const __m128i is_left            = _mm_cmpeq_epi8(rights,          zero);
+  const __m128i vecwid_for_left    = _mm_and_si128 (is_left,         vec_widths);
+  const __m128i ns_for_shufmask    = _mm_or_si128  (nslo,            vecwid_for_left);
 
-  __m128i sse_inc = _mm_setzero_si128();
+  const __m128i shufmask1_right    = _mm_add_epi8  (ns_for_shufmask, rights);
+  const __m128i shufmask1          = _mm_sub_epi8  (shufmask1_right, lefts);
+
+  const __m128i md2bimask          = _mm_cmpgt_epi8(vec_widths,      shufmask1);
+  const __m128i move_d_to_b_imask  = _mm_or_si128  (is_left,         md2bimask);
+  const __m128i move_b_to_d_mask   = _mm_cmpgt_epi8(lefts,           nslo);
+
+  // If we're straddling the left border, start from the left border instead,
+  // and if right border, end on the border
+  const int32_t ld_offset = left - right;
+
   int32_t y;
+  __m128i sse_inc = _mm_setzero_si128();
   for (y = 0; y < height_twoline_groups; y += 2) {
     __m128i a = _mm_loadu_si128((__m128i *)(pic_data + (y + 0) * pic_stride + 0));
-    __m128i b = _mm_loadu_si128((__m128i *)(ref_data + (y + 0) * ref_stride + 0));
+    __m128i b = _mm_loadu_si128((__m128i *)(ref_data + (y + 0) * ref_stride + 0  + ld_offset));
     __m128i c = _mm_loadu_si128((__m128i *)(pic_data + (y + 0) * pic_stride + 16));
-    __m128i d = _mm_loadu_si128((__m128i *)(ref_data + (y + 0) * ref_stride + 16));
+    __m128i d = _mm_loadu_si128((__m128i *)(ref_data + (y + 0) * ref_stride + 16 + ld_offset));
     __m128i e = _mm_loadu_si128((__m128i *)(pic_data + (y + 1) * pic_stride + 0));
-    __m128i f = _mm_loadu_si128((__m128i *)(ref_data + (y + 1) * ref_stride + 0));
+    __m128i f = _mm_loadu_si128((__m128i *)(ref_data + (y + 1) * ref_stride + 0  + ld_offset));
     __m128i g = _mm_loadu_si128((__m128i *)(pic_data + (y + 1) * pic_stride + 16));
-    __m128i h = _mm_loadu_si128((__m128i *)(ref_data + (y + 1) * ref_stride + 16));
+    __m128i h = _mm_loadu_si128((__m128i *)(ref_data + (y + 1) * ref_stride + 16 + ld_offset));
 
-    __m128i border_px_lo = _mm_set1_epi8  (*(uint8_t *)(ref_data + (y + 0) * ref_stride + border_pix_off));
-    __m128i border_px_hi = _mm_set1_epi8  (*(uint8_t *)(ref_data + (y + 1) * ref_stride + border_pix_off));
-    __m128i b_epol       = _mm_blendv_epi8(b, border_px_lo, epol_masklo);
-    __m128i d_epol       = _mm_blendv_epi8(d, border_px_lo, epol_maskhi);
-    __m128i f_epol       = _mm_blendv_epi8(f, border_px_hi, epol_masklo);
-    __m128i h_epol       = _mm_blendv_epi8(h, border_px_hi, epol_maskhi);
+    __m128i b_shifted         = _mm_shuffle_epi8(b, shufmask1);
+    __m128i d_shifted         = _mm_shuffle_epi8(d, shufmask1);
+    __m128i f_shifted         = _mm_shuffle_epi8(f, shufmask1);
+    __m128i h_shifted         = _mm_shuffle_epi8(h, shufmask1);
 
-    __m128i curr_sads_ab = _mm_sad_epu8(a, b_epol);
-    __m128i curr_sads_cd = _mm_sad_epu8(c, d_epol);
-    __m128i curr_sads_ef = _mm_sad_epu8(e, f_epol);
-    __m128i curr_sads_gh = _mm_sad_epu8(g, h_epol);
+    // TODO: could these be optimized for two-operand efficiency? Only one of
+    // these ever does useful work, the other should leave the vector untouched,
+    // so could the first result be used in the second calculation or something?
+    __m128i b_with_d_data     = _mm_blendv_epi8(d_shifted, b_shifted, move_d_to_b_imask);
+    __m128i d_with_b_data     = _mm_blendv_epi8(d_shifted, b_shifted, move_b_to_d_mask);
+    __m128i f_with_h_data     = _mm_blendv_epi8(h_shifted, f_shifted, move_d_to_b_imask);
+    __m128i h_with_f_data     = _mm_blendv_epi8(h_shifted, f_shifted, move_b_to_d_mask);
+
+    __m128i b_final           = _mm_shuffle_epi8(b_with_d_data, epol_mask_lo);
+    __m128i d_final           = _mm_shuffle_epi8(d_with_b_data, epol_mask_hi);
+    __m128i f_final           = _mm_shuffle_epi8(f_with_h_data, epol_mask_lo);
+    __m128i h_final           = _mm_shuffle_epi8(h_with_f_data, epol_mask_hi);
+
+    __m128i curr_sads_ab      = _mm_sad_epu8    (a, b_final);
+    __m128i curr_sads_cd      = _mm_sad_epu8    (c, d_final);
+    __m128i curr_sads_ef      = _mm_sad_epu8    (e, f_final);
+    __m128i curr_sads_gh      = _mm_sad_epu8    (g, h_final);
 
     sse_inc = _mm_add_epi64(sse_inc, curr_sads_ab);
     sse_inc = _mm_add_epi64(sse_inc, curr_sads_cd);
@@ -142,21 +168,25 @@ static uint32_t hor_sad_sse41_w32(const kvz_pixel *pic_data, const kvz_pixel *re
   }
   if (height_residual_lines) {
     __m128i a = _mm_loadu_si128((__m128i *)(pic_data + (y + 0) * pic_stride + 0));
-    __m128i b = _mm_loadu_si128((__m128i *)(ref_data + (y + 0) * ref_stride + 0));
+    __m128i b = _mm_loadu_si128((__m128i *)(ref_data + (y + 0) * ref_stride + 0  + ld_offset));
     __m128i c = _mm_loadu_si128((__m128i *)(pic_data + (y + 0) * pic_stride + 16));
-    __m128i d = _mm_loadu_si128((__m128i *)(ref_data + (y + 0) * ref_stride + 16));
+    __m128i d = _mm_loadu_si128((__m128i *)(ref_data + (y + 0) * ref_stride + 16 + ld_offset));
 
-    __m128i border_px = _mm_set1_epi8  (*(uint8_t *)(ref_data + (y + 0) * ref_stride + border_pix_off));
-    __m128i b_epol    = _mm_blendv_epi8(b, border_px, epol_masklo);
-    __m128i d_epol    = _mm_blendv_epi8(d, border_px, epol_maskhi);
+    __m128i b_shifted         = _mm_shuffle_epi8(b, shufmask1);
+    __m128i d_shifted         = _mm_shuffle_epi8(d, shufmask1);
 
-    __m128i curr_sads_ab = _mm_sad_epu8(a, b_epol);
-    __m128i curr_sads_cd = _mm_sad_epu8(c, d_epol);
+    __m128i b_with_d_data     = _mm_blendv_epi8(d_shifted, b_shifted, move_d_to_b_imask);
+    __m128i d_with_b_data     = _mm_blendv_epi8(d_shifted, b_shifted, move_b_to_d_mask);
+
+    __m128i b_final           = _mm_shuffle_epi8(b_with_d_data, epol_mask_lo);
+    __m128i d_final           = _mm_shuffle_epi8(d_with_b_data, epol_mask_hi);
+
+    __m128i curr_sads_ab      = _mm_sad_epu8    (a, b_final);
+    __m128i curr_sads_cd      = _mm_sad_epu8    (c, d_final);
 
     sse_inc = _mm_add_epi64(sse_inc, curr_sads_ab);
     sse_inc = _mm_add_epi64(sse_inc, curr_sads_cd);
   }
-
   __m128i sse_inc_2 = _mm_shuffle_epi32(sse_inc, _MM_SHUFFLE(1, 0, 3, 2));
   __m128i sad       = _mm_add_epi64    (sse_inc, sse_inc_2);
   return _mm_cvtsi128_si32(sad);
