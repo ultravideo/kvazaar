ultravideo/kvazaar

commit 1c36f68d0c777f88fda9cdcf63229a41415e3fe0

Fix owf>=9 gop=8 and add test to catch such problem in future

src/encoderstate.c
@@ -802,6 +802,20 @@ static void encoder_state_encode_leaf(encoder_state_t * const state)
             dep_lcu = dep_lcu->right;
           }
           kvz_threadqueue_job_dep_add(job[0], ref_state->tile->wf_jobs[dep_lcu->id]);
+
+          // Very spesific bug that happens when owf length is longer than the
+          // gop length. Takes care of that.
+          if(state->encoder_control->cfg.gop_lowdelay &&
+             !state->encoder_control->cfg.open_gop &&
+             state->encoder_control->cfg.owf > state->encoder_control->cfg.gop_len &&
+             ref_state->frame->slicetype == KVZ_SLICE_I &&
+             ref_state->frame->num != 0){
+
+            while (ref_state->frame->poc != state->frame->poc - 8){
+              ref_state = ref_state->previous_encoder_state;
+            }
+            kvz_threadqueue_job_dep_add(job[0], ref_state->tile->wf_jobs[dep_lcu->id]);
+          }
         }
 
         // Add local WPP dependancy to the LCU on the left.

tests/test_gop.sh
@@ -12,6 +12,7 @@ valgrind_test 264x130 20 $common_args --gop=8 -p16 --owf=0
 valgrind_test 264x130 10 $common_args --gop=8 -p1 --owf=4
 valgrind_test 264x130 10 $common_args --gop=lp-g4d3t1 -p5 --owf=4
 valgrind_test 264x130 10 $common_args --gop=8 -p8 --owf=4 --no-open-gop
+valgrind_test 264x130 30 $common_args --gop=8 -p16 --owf=16
 # Do more extensive tests in a private gitlab CI runner
 if [ ! -z ${GITLAB_CI+x} ];then valgrind_test 264x130 20 $common_args --gop=8 -p8 --owf=0 --no-open-gop; fi
 if [ ! -z ${GITLAB_CI+x} ];then valgrind_test 264x130 40 $common_args --gop=8 -p32 --owf=4 --no-open-gop; fi

commit 977e885ea2fd0386449541d387fa0494b3db98e9

Fix issue with gop=0 introduced in 1c36f68d0c777f88fda9cdcf63229a41415e3fe0

src/encoderstate.c
@@ -807,6 +807,7 @@ static void encoder_state_encode_leaf(encoder_state_t * const state)
           // gop length. Takes care of that.
           if(!state->encoder_control->cfg.gop_lowdelay &&
              state->encoder_control->cfg.open_gop &&
+             state->encoder_control->cfg.gop_len != 0 &&
              state->encoder_control->cfg.owf > state->encoder_control->cfg.gop_len &&
              ref_state->frame->slicetype == KVZ_SLICE_I &&
              ref_state->frame->num != 0){