21c07638ed33ca38b8421775e9262ebfdf6f66bb
mercat <alexandre.mercat@tut.fi>
2019-09-11 15:57:15 +0300

    Remove const into kvz_init_constraint.

--- src/constraint.c
+++ src/constraint.c
@@ -26,7 +26,7 @@
   * \param state   encoder state
   * \return the pointer of constraint_t structure
   */
-void * kvz_init_constraint(encoder_state_t* state, encoder_control_t * const encoder) {
+void * kvz_init_constraint(encoder_state_t* state, encoder_control_t * encoder) {
 	constraint_t* constr = NULL;
 	// Allocate the constraint_t strucutre
 	constr = MALLOC(constraint_t, 1);
