d6f85d576d1f2637e4c3fd756a533231a5c803ca
Jaakko Laitinen <jaakko.laitinen@tut.fi>
2019-11-04 15:29:16 +0200

    Update tests to result in standard compliant bitstream

--- tests/test_scalability_tiles.sh
+++ tests/test_scalability_tiles.sh
@@ -6,9 +6,9 @@ set -eu
 . "${0%/*}/util.sh"
 
 #Test tiles
-valgrind_test 512x264 20 --preset=ultrafast -p12 --layer-res=256x132 -q30 -r3 --gop=0 --tiles=2x2 --layer --preset=ultrafast -q28 -r2 --ilr=1 --gop=0 --tiles=2x2
-valgrind_test 512x264 20 --preset=ultrafast -p12 -q30 -r3 --gop=0 --tiles=2x2 --layer --preset=ultrafast -q28 -r2 --ilr=1 --gop=0 --tiles=2x2
+valgrind_test 1024x1024 20 --preset=ultrafast -p12 --layer-res=512x512 -q30 -r3 --gop=0 --tiles=2x2 --layer --preset=ultrafast -q28 -r2 --ilr=1 --gop=0 --tiles=2x2
+valgrind_test 1024x1024 20 --preset=ultrafast -p12 -q30 -r3 --gop=0 --tiles=2x2 --layer --preset=ultrafast -q28 -r2 --ilr=1 --gop=0 --tiles=2x2
 
 # Test without threads
-valgrind_test 512x264 20 --preset=ultrafast -p12 --layer-res=256x132 -q30 -r3 --gop=0 --tiles=2x2 --layer --preset=ultrafast -q28 -r2 --ilr=1 --gop=0 --threads=0 --owf=0 --tiles=2x2
-valgrind_test 512x264 20 --preset=ultrafast -p12 -q30 -r3 --gop=0 --tiles=2x2 --layer --preset=ultrafast -q28 -r2 --ilr=1 --gop=0 --threads=0 --owf=0 --tiles=2x2
+valgrind_test 1024x1024 20 --preset=ultrafast -p12 --layer-res=512x512 -q30 -r3 --gop=0 --tiles=2x2 --layer --preset=ultrafast -q28 -r2 --ilr=1 --gop=0 --threads=0 --owf=0 --tiles=2x2
+valgrind_test 1024x1024 20 --preset=ultrafast -p12 -q30 -r3 --gop=0 --tiles=2x2 --layer --preset=ultrafast -q28 -r2 --ilr=1 --gop=0 --threads=0 --owf=0 --tiles=2x2
