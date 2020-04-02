067611d9cfd20f17930035cd57f8441c2c530973
siivonek <kari.siivonen@tuni.fi>
2020-04-02 11:02:58 +0200

    Add tests for VAQ with both rate controls.

--- tests/test_tools.sh
+++ tests/test_tools.sh
@@ -11,3 +11,5 @@ valgrind_test $common_args --no-rdoq --no-deblock --no-sao --no-signhide --subme
 valgrind_test $common_args --no-rdoq --no-signhide --subme=0
 valgrind_test $common_args --rdoq --no-deblock --no-sao --subme=0
 valgrind_test $common_args --vaq=8
+valgrind_test $common_args --vaq=8 --bitrate 3500
+valgrind_test $common_args --vaq=8 --rc-algorithm oba --bitrate 3500
