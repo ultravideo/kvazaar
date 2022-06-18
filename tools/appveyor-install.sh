c449a14e20eb875b644914e482f8f76ea89b3522
Marko Viitanen <fador@iki.fi>
2022-06-18 14:23:47 +0300

    [CI] Fix AppVeyor builds

--- tools/appveyor-install.sh
+++ tools/appveyor-install.sh
@@ -4,7 +4,8 @@ set -e
 # Install build dependencies for kvazaar
 pacman -S --noconfirm --noprogressbar --needed \
     $MINGW_PACKAGE_PREFIX-gcc \
-    $MINGW_PACKAGE_PREFIX-yasm
+    $MINGW_PACKAGE_PREFIX-yasm \
+	$MINGW_PACKAGE_PREFIX-autotools
 
 # Delete unused packages to reduce space used in the Appveyor cache
 pacman -Sc --noconfirm
