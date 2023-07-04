cf24612a49905f40230501f14b738301543e8c2a
Marko Viitanen <fador@iki.fi>
2023-07-04 20:54:47 +0300

    [crypto] Add a test for the crypto

--- tests/test_tools.sh
+++ tests/test_tools.sh
@@ -13,3 +13,4 @@ valgrind_test $common_args --rdoq --no-deblock --no-sao --subme=0
 valgrind_test $common_args --vaq=8
 valgrind_test $common_args --vaq=8 --bitrate 3500
 valgrind_test $common_args --vaq=8 --rc-algorithm oba --bitrate 3500
+valgrind_test $common_args --crypto=on --hash=none
