e0ee324892316da03266fdba3a650b33b50bddd7
Marko Viitanen <fador@iki.fi>
2025-09-16 12:15:15 +0300

    Add tests for 4:4:4 chroma subsampling

--- tests/util.sh
+++ tests/util.sh
@@ -18,6 +18,42 @@ print_and_run() {
     "$@"
 }
 
+prepare_444() {
+    cleanup
+    print_and_run \
+        ffmpeg -f lavfi -i "mandelbrot=size=${1}" \
+            -vframes "${2}" -pix_fmt yuv444p -f yuv4mpegpipe \
+            "${yuvfile}"
+}
+
+valgrind_test_444() {
+    dimensions="$1"
+    shift
+    frames="$1"
+    shift
+
+    prepare_444 "${dimensions}" "${frames}"
+
+    # If $KVZ_TEST_VALGRIND is defined and equal to "1", run the test with
+    # valgrind. Otherwise, run without valgrind.
+    if [ "${KVZ_TEST_VALGRIND:-0}" = '1' ]; then
+        valgrind='valgrind --leak-check=full --error-exitcode=1 --'
+    else
+        valgrind=''
+    fi
+
+    # No quotes for $valgrind because it expands to multiple (or zero)
+    # arguments.
+    print_and_run \
+        ../libtool execute $valgrind \
+            ../src/kvazaar -i "${yuvfile}" --input-format=P444 "--input-res=${dimensions}" -o "${hevcfile}" "$@"
+
+    print_and_run \
+        TAppDecoderStatic -b "${hevcfile}"
+
+    cleanup
+}
+
 prepare() {
     cleanup
     print_and_run \
